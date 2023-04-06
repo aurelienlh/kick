913b1724704945d171fb4160aad799b7538845ed
duchatensiie <aurelienlhuillier75@gmail.com>
2023-04-07 00:47:37 +0200

    kickclient

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId,ThirdwebProvider } from "@thirdweb-dev/react";
+import './index.css'
+import App from "./App";
+import { StateContextProvider } from "./context";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+        <Router>
+            <StateContextProvider>
+            <App />
+            </StateContextProvider>
+        </Router>
+    </ThirdwebProvider>
+)
---
+++ src/pages/Profie.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+import DisplayCampaigns from '../conponents/DisplayCampaigns';
+import { useStateContext } from '../context'
+
+const Profile = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+  const {address, contract, getUserCampaigns} =useStateContext();
+  const fetchCampaigns =async () => {
+    setIsLoading(true);
+    const data=await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+  useEffect(()=> {
+    if(contract) fetchCampaigns();
+  } , [address,contract]);
+  return (
+    <DisplayCampaigns
+    title="All Campaign"
+    isLoading={isLoading}
+    campaigns={campaigns} />
+
+  )
+}
+
+export default Profile
